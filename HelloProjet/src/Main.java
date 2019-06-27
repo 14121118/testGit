e0b301c642025636b9099cf3acab5f3e606dcd9c
kqm <15870671008@163.com>
2019-06-27 15:56:38 +0800

    修改1

--- HelloProjet/src/Main.java
+++ HelloProjet/src/Main.java
@@ -5,5 +5,7 @@ public class Main {
     @Test
     public void testHelloWrold() {
         System.out.println("HelloWorld!");
+        System.out.println("HelloWorld2!");
+        System.out.println("HelloWorld3!");
     }
 }
